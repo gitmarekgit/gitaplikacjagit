0e75f3835eae1190ccd10bb207034d16cc829218
admin-gr02 <admin-gr02@AM-T-07481>
2023-10-24 08:46:24 +0200

    adasdas

--- gitprojektgit/src/main/java/com/example/gitprojektgit/HelloController.java
+++ gitprojektgit/src/main/java/com/example/gitprojektgit/HelloController.java
@@ -2,13 +2,28 @@ package com.example.gitprojektgit;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField nazwaUzytkownika;
+    @FXML
+    private TextField haslo;
+    @FXML
+    private Label text;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("siema!");
+    protected void zaloguj()
+    {
+        String nazwa = "Marek";
+        String haslo1 = "marek123";
+
+        if(nazwaUzytkownika.equals(nazwa) && haslo.equals(haslo)){
+            text.setText("Zalogowales sie!");
+        }
+        else{
+            text.setText("Zle dane :(");
+        }
+
     }
 }
