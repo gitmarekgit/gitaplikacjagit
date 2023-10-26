d6d332904ca6d1f999551b399541ef493a4da2c1
Marek <marekksiazczyk23@gmail.com>
2023-10-26 10:36:59 +0200

    login

--- gitprojektgit/src/main/java/com/example/gitprojektgit/HelloApplication.java
+++ gitprojektgit/src/main/java/com/example/gitprojektgit/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 384, 225);
+        stage.setTitle("gitprojektgit");
         stage.setScene(scene);
         stage.show();
     }
