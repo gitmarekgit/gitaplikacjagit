960df9d4e6959e996fd876e9a5f406b2c4beb7c0
Marek <marekksiazczyk23@gmail.com>
2023-10-30 12:24:08 +0100

    scena z logo

--- gitprojektgit/src/main/java/com/example/gitprojektgit/Main.java
+++ gitprojektgit/src/main/java/com/example/gitprojektgit/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
         mainStage = stage;
         mainStage.setResizable(false);
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 600 , 400);
 
         stage.setTitle("gitprojektgit");
         stage.setScene(scene);
